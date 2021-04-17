888706397b2de288c7476ce7146db93ce29c3b3b
Sergei Lapin <macmeprag@gmail.com>
2021-04-17 15:21:46 +0300

    update sample so as to show text blurred

--- sample/src/main/java/com/slapin/blurview/sample/MainActivity.kt
+++ sample/src/main/java/com/slapin/blurview/sample/MainActivity.kt
@@ -15,11 +15,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val img = findViewById<View>(R.id.img)
+        val blurred = findViewById<View>(R.id.blurred)
+        val notBlurred = findViewById<View>(R.id.notBlurred)
+        val views = listOf(img, blurred, notBlurred)
         animator = ValueAnimator.ofFloat(1f, 2f).apply {
             addUpdateListener { animator ->
                 val animatedValue = animator.animatedValue as Float
-                img.scaleX = animatedValue
-                img.scaleY = animatedValue
+                views.forEach { view ->
+                    view.scaleX = animatedValue
+                    view.scaleY = animatedValue
+                }
             }
             duration = 3000L
             repeatCount = INFINITE
